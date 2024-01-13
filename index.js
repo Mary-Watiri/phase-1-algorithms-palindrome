268231d18b71f451e4b9b19ab91412e921617af9
Mary Watiri <watirimary30@gmail.com>
2024-01-13 22:12:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  var reversed = word.split("").reverse().join("");
 
-/* 
-  Add your pseudocode here
+  if (reversed === word){
+    return true;
+  }else{
+    return false;
+  }
+
+}
+const word = "abb";
+isPalindrome(word);
+/* START
+  Create a function isPalindrome
+  The function should take in one parameter
+  Declare a variable that reverses the original word passed in
+  Use the if else statements to compare the reversed word with the original word
+  Declare the variable passed in as a parameter in the function and assign it a value
+  Call the function isPalindrome
+  END
 */
 
 /*
-  Add written explanation of your solution here
+  My code takes in any word in lowercase as an input,my function isPalindrome reverses the word and compares whether it is equal to the original and returns true if the two are equal.
 */
 
 // You can run `node index.js` to view these console logs
